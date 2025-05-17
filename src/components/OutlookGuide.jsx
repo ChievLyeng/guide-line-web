9baa7b5b3b7c943b96c3b4ef2582781b2e49fd7d
Lyeng <lyeng2210@gmial.com>
2025-05-17 14:54:29 +0700

    done outlook

--- src/components/OutlookGuide.jsx
+++ src/components/OutlookGuide.jsx
@@ -1,4 +1,8 @@
 import StepCard from "./StepCard";
+import SingIn from "../img/outlook/signin.png";
+import Email from "../img/outlook/email.png";
+import Verify from "../img/outlook/verify.png";
+import Done from "../img/outlook/done.png";
 
 const OutlookGuide = () => {
   return (
@@ -6,23 +10,23 @@ const OutlookGuide = () => {
       <h2 className="mb-4">Microsoft Outlook Setup</h2>
       <StepCard
         step="1"
-        description="Open Microsoft Outlook. If it's your first time, it will prompt for email setup."
-        image="/images/outlook-step1.png"
+        description="Open Microsoft Official Website https://www.microsoft.com find the outlook tap anc click on it then click on sign in button to sign in."
+        image={SingIn}
       />
       <StepCard
         step="2"
-        description="Enter chievlyeng@gmail.com and click Connect."
-        image="/images/outlook-step2.png"
+        description="When You click on sign in it will link you to this form and you have to input your gmail account, microsoft accout or phone. So here I use gmail then I enter my gamil accout and click next."
+        image={Email}
       />
       <StepCard
         step="3"
-        description="Enter your Gmail password when prompted and approve access."
-        image="/images/outlook-step3.png"
+        description="So this step is require for you to verify you account if use gmail get the code that send to gmail for the verification."
+        image={Verify}
       />
       <StepCard
         step="4"
-        description="Click Done and Outlook will sync your email."
-        image="/images/outlook-step4.png"
+        description="When verified account already you will be logged in to your out look and can start using it."
+        image={Done}
       />
     </div>
   );
