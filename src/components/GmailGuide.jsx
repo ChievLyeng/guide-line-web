2da046bb4e46def8c2d705a7ce2fece16bd9ea3d
Lyeng <lyeng2210@gmial.com>
2025-05-17 15:03:51 +0700

    finish email

--- src/components/GmailGuide.jsx
+++ src/components/GmailGuide.jsx
@@ -1,4 +1,7 @@
 import StepCard from "./StepCard";
+import Login from "../img/gmail/login.png";
+import Password from "../img/gmail/password.png";
+import Done from "../img/gmail/done.png";
 
 const GmailGuide = () => {
   return (
@@ -6,23 +9,18 @@ const GmailGuide = () => {
       <h2 className="mb-4">Using Gmail (Web Version)</h2>
       <StepCard
         step="1"
-        description="Go to https://mail.google.com/ and sign in."
-        image="/images/gmail-step1.png"
+        description="Go to https://mail.google.com and you see the form if you already have gmail account you can login by enter you account if not you have to sign up."
+        image={Login}
       />
       <StepCard
         step="2"
-        description="Enter your Gmail address: chievlyeng@gmail.com and your password."
-        image="/images/gmail-step2.png"
+        description="After entered you gmail account you have to enter you password then click next button."
+        image={Password}
       />
       <StepCard
         step="3"
-        description="After logging in, you'll be in your Gmail inbox."
-        image="/images/gmail-step3.png"
-      />
-      <StepCard
-        step="4"
-        description="Click 'Compose' to write and send a new email."
-        image="/images/gmail-step4.png"
+        description="So after logged in you gmail and password now you can use gmail for email sender."
+        image={Done}
       />
     </div>
   );
