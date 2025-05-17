878242a45e684f395183d73e59754552854eab05
Lyeng <lyeng2210@gmial.com>
2025-05-17 14:37:03 +0700

    finish thunderbird

--- src/components/ThunderbirdGuide.jsx
+++ src/components/ThunderbirdGuide.jsx
@@ -1,4 +1,8 @@
 import StepCard from "./StepCard";
+import ThunderDownloadImg from "../img/thunderbird/install.png";
+import ThunderSetupImg from "../img/thunderbird/setup.png";
+import ThunderCreateImg from "../img/thunderbird/create.png";
+import ThunderDoneImg from "../img/thunderbird/done.png";
 
 const ThunderbirdGuide = () => {
   return (
@@ -6,23 +10,23 @@ const ThunderbirdGuide = () => {
       <h2 className="mb-4">Mozilla Thunderbird Setup</h2>
       <StepCard
         step="1"
-        description="Go to the official Thunderbird website: https://www.thunderbird.net/"
-        image="/images/thunderbird-step1.png"
+        description={`Go to the official Thunderbird website: https://www.thunderbird.net and Click on Dowonload. It will download to you device.`}
+        image={ThunderDownloadImg}
       />
       <StepCard
         step="2"
-        description="Download and install Thunderbird for your OS."
-        image="/images/thunderbird-step2.png"
+        description="Find the downloaded file in you pc and click install. As in the photo click ok to install then just follow the default installation. You will see the launch app as below image."
+        image={ThunderSetupImg}
       />
       <StepCard
         step="3"
-        description="Open Thunderbird and choose 'Set up an Existing Email Account'."
-        image="/images/thunderbird-step3.png"
+        description="After install and launch the app now you have to register the account with you gmail accout. So input your username gmail and your password then click continue. When you clicked on continue button it will show a form to login you gmail accout so you have to login and verify gmail account."
+        image={ThunderCreateImg}
       />
       <StepCard
         step="4"
-        description="Enter your name, email (chievlyeng@gmail.com), and password."
-        image="/images/thunderbird-step4.png"
+        description="After logged In and verified gmail account you will be able to use the app. So Mozilla Thunderbird is successfully set up."
+        image={ThunderDoneImg}
       />
     </div>
   );
